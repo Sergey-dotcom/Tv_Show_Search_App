b20442308177e633985a0f767c9d245c69675ee1
Sergey-dotcom <sergeyovsynnikov2@gmail.com>
2021-10-20 12:03:32 +0300

    Updates

---
+++ precache-manifest.02db1fb8276d39a4d2ac8287759d6ee3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0df61ab188273006af34cb122a35a430",
+    "url": "/Tv_Show_Search_App/index.html"
+  },
+  {
+    "revision": "87ce28e966b86573b840",
+    "url": "/Tv_Show_Search_App/static/css/main.151f1da3.chunk.css"
+  },
+  {
+    "revision": "9473d6e00a60c256cbca",
+    "url": "/Tv_Show_Search_App/static/js/2.49d70228.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/Tv_Show_Search_App/static/js/2.49d70228.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c49ff4966ee9635846b7",
+    "url": "/Tv_Show_Search_App/static/js/3.bc0d7f2b.chunk.js"
+  },
+  {
+    "revision": "87ce28e966b86573b840",
+    "url": "/Tv_Show_Search_App/static/js/main.3ca5145b.chunk.js"
+  },
+  {
+    "revision": "7aefccb84499e9a37666",
+    "url": "/Tv_Show_Search_App/static/js/runtime-main.24aaba22.js"
+  },
+  {
+    "revision": "b99b50f734473ca37b25645fa315fbc9",
+    "url": "/Tv_Show_Search_App/static/media/spin.b99b50f7.gif"
+  }
+]);
